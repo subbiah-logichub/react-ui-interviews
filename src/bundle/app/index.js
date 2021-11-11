6a5e20bcf4efc6cd332af9327043e10b16a05c5c
subbiah chandramouli <58955584+subbiah-logichub@users.noreply.github.com>
2021-11-11 00:12:20 -0600

    removing existing code

--- src/bundle/app/index.js
+++ src/bundle/app/index.js
@@ -1,44 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import axios from "axios";
 import { Container, Section, Bar } from "react-simple-resizer";
-
-import { Tabs } from "../../components/tabs";
 
 import "./index.scss";
 
 export function App() {
-  const [tabs, setTabs] = useState([]);
-  const [isTabsLoading, setIsTabsLoading] = useState(true);
-  const [isTabsError, setIsTabsError] = useState("");
-
-  useEffect(() => {
-    setTimeout(() => {
-      axios
-        .get(
-          `${window.location.protocol}//${window.location.host}/mock-data/tabs.json`
-        )
-        .then((result) => {
-          setTabs(result.data);
-        })
-        .catch((error) => {
-          setIsTabsError(error.message);
-        })
-        .finally(() => {
-          setIsTabsLoading(false);
-        });
-    }, 2000);
-  }, []);
-
   return (
     <Container className="app">
       <Section className="app-nav" minSize={40} defaultSize={210} />
       <Bar size={10} className="app-resizer" />
-      <Section className="app-view" minSize={100}>
-        {isTabsLoading && "loading..."}
-        {isTabsError && !isTabsLoading && isTabsError}
-        {!isTabsError && !isTabsLoading && <Tabs tabs={tabs} />}
-      </Section>
+      <Section className="app-view" minSize={100} />
     </Container>
   );
 }
